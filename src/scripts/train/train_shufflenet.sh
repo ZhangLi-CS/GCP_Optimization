db0f8f50aa212511228a7c3b60adcec288841dec
ZhangLi-CS <61503078+ZhangLi-CS@users.noreply.github.com>
2020-03-11 20:00:34 +0800

    Add files via upload

---
+++ src/scripts/train/train_shufflenet.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+now=$(date +"%Y%m%d_%H%M%S")
+jobname=ShuffleNet_V2_x1_0_LRnorm
+log_dir=logs/${jobname}
+if [ ! -d $log_dir ]; then
+  echo create log $log_dir
+  mkdir -p $log_dir
+fi
+
+python  -u main.py \
+	-a shufflenet_v2_X1_0 \
+	--lr_mode LRnorm \
+        -p 500 \
+        -j 32 \
+        --resume checkpoint.pth.tar  \
+	/home/sdc1/dataset/ILSVRC2012/images | tee ./logs/${jobname}/record-train-${now}.txt \
+
