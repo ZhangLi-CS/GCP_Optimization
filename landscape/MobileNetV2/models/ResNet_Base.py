0beeff743a1f13df227a0a9ba75fa545f52652f1
ZhangLi-CS <61503078+ZhangLi-CS@users.noreply.github.com>
2020-03-11 20:28:57 +0800

    Add files via upload

---
+++ landscape/MobileNetV2/models/ResNet_Base.py
@@ -0,0 +1,55 @@
+import torch
+import torch.nn as nn
+from .utils import load_state_dict_from_url
+
+
+__all__ = ['ResNet_Base','resnet_base']
+
+
+
+class ResNet_Base(nn.Module):
+
+    def __init__(self):
+        super(ResNet_Base, self).__init__()
+        
+        norm_layer = nn.BatchNorm2d
+        self._norm_layer = norm_layer
+
+        self.inplanes = 64
+
+        self.conv1 = nn.Conv2d(3, self.inplanes, kernel_size=7, stride=2, padding=3,
+                               bias=False)
+        self.bn1 = norm_layer(self.inplanes)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+        # Zero-initialize the last BN in each residual branch,
+        # so that the residual branch starts with zeros, and each residual block behaves like an identity.
+        # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        return x
+
+
+def _resnet_base(**kwargs):
+
+    model = ResNet_Base(**kwargs)
+    return model
+
+def resnet_base(**kwargs):
+    return _resnet_base()
+
+
+
+
